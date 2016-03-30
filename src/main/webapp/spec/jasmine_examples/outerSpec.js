fe3424c6fad2cfaa5cd3053449fc6d917fafe77b
satya2363 <subbu2363@gmail.com>
2016-03-30 16:24:08 -0400

    Jasmine tests locally
    Only spec folders will be tested.

---
+++ src/main/webapp/spec/jasmine_examples/outerSpec.js
@@ -0,0 +1,15 @@
+/**
+ * Created by subbaraju on 30/3/2016.
+ */
+describe('outer', function() {
+    'use strict';
+
+    it('boolean check', function() {
+        expect(true).toBe(true);
+    });
+
+    it('integer check', function() {
+        expect(2).toBe(0);
+    });
+
+})
