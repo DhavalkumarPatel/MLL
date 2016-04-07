df91225bcd67addca4742046e73c353185b69ba2
satya2363 <subbu2363@gmail.com>
2016-04-06 20:06:43 -0400

    Registration

--- src/main/webapp/dist/scripts/app-0605091d5a.js
+++ src/main/webapp/dist/scripts/app-0605091d5a.js
@@ -401,9 +401,24 @@
 
     function UserRegistrationFormController() {
 
+        this.genres = ['Alternative', 'Blues', 'Children\'s Music', 'Christian & Gospel', 'Comedy', 'Classical',
+            'Country', 'Dance', 'Electronic', 'Hip - Hop / Rap', 'Pop', 'Jazz', 'Latino', 'R & B / Soul', 'Reggae',
+            'Metal', 'Rock', 'Singer / Songwriter', 'Folk / Americana', 'Funk' ];
+
+
+
+        this.selectGenre = (genre) => { if(!genre) this.data.twoGenre = null; };
+
+        this.genders = [{label:'MALE',id:'1'},{label:'FEMALE',id:'2'}];
+
+        this.selectGender = {label:'MALE',id:'1'}; //default
+
         this.submit = () => {
-            if (this.generalForm.$invalid) this.generalForm.$submitted = true;
-            else this.onNext();
+            if (this.registerForm.$invalid) this.registerForm.$submitted = true;
+            else {
+                alert("form successful");
+                this.onNext();
+            }
         };
 
 
