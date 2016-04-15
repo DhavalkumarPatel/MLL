52a66f4e0a54ee34641d8fcea00f09b8f52a2f92
dhavalpatel <patel.dha@husky.neu.edu>
2016-04-15 19:06:08 -0400

    commented Nuxeo integration code

--- src/main/java/mll/service/NuxeoService.java
+++ src/main/java/mll/service/NuxeoService.java
@@ -2,6 +2,15 @@ package mll.service;
 
 import java.util.List;
 
+/*
+import org.nuxeo.ecm.automation.client.Constants;
+import org.nuxeo.ecm.automation.client.Session;
+import org.nuxeo.ecm.automation.client.adapters.DocumentService;
+import org.nuxeo.ecm.automation.client.jaxrs.impl.HttpAutomationClient;
+import org.nuxeo.ecm.automation.client.model.Document;
+import org.nuxeo.ecm.automation.client.model.PathRef;
+*/
+
 import mll.beans.Metadata;
 import mll.utility.Configuration;
 
@@ -34,21 +43,19 @@ public class NuxeoService
 				{
 					if(null != metadata && null != metadata.getSong() && null != metadata.getSong().getId())
 					{
+						/*
 						// Fetch the root of Nuxeo repository
-						/*Document root = (Document) session.newRequest("Document.Fetch").set("value", "/").execute();
-						
-						File file=new File("C:\\song.mp3");
-						FileBlob fb = new FileBlob(file);
-						fb.setMimeType("audio/mp3");
+						Document root = (Document) session.newRequest("Document.Fetch").set("value", "/").execute();
 						
 						// create a file document
 						session.newRequest("Document.Create").setInput(root).set("type", "File").set("name", "myfile"+metadata.getSong().getId()).set("properties", "dc:title=MLL-SONG "+metadata.getSong().getId()).execute();
-						session.newRequest("Blob.Attach").setHeader(Constants.HEADER_NX_VOIDOP, "true").setInput(fb).set("document", "/myfile"+metadata.getSong().getId()).execute();
+						session.newRequest("Blob.Attach").setHeader(Constants.HEADER_NX_VOIDOP, "true").setInput(metadata.getSong().getContent()).set("document", "/myfile"+metadata.getSong().getId()).execute();
 						
 						//Fetch the document for JSON
 						Document document = (Document) session.newRequest(DocumentService.GetDocumentChild).setInput(new PathRef("/")).set("name", "myfile"+metadata.getSong().getId()).execute();
 						document.set("dc:description", metadata.getMetadataJson().toString());
-						document = (Document) session.newRequest("Document.Update").setInput(document).set("properties", document).execute();*/
+						document = (Document) session.newRequest("Document.Update").setInput(document).set("properties", document).execute();
+						*/
 					}
 				}
 			}
